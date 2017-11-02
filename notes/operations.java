59fc9995987ff62539cea90d58adc9713e830b6b
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-11-02 11:06:38 -0400

    project notes built into code

---
+++ notes/operations.java
@@ -0,0 +1,24 @@
+
+ String[] toppings =  new String[20];
+ formats3 
+
+
+
+ discover_format_four()
+ discover_format_three()
+ discover_format_two()
+ discover_format_one()
+
+each line has ... max three words
+each line is its own object ... -said by the book.
+//import Assembler_Application
+// class source_line
+//	String symbol, mnemonic, label
+//
+//
+// addresses(pass1) opcode(pass2)-- output variables 
+
+first_pass()//- go through and create the objects. also create the addresses
+
+//class Assembler_Application
+LOCCTR() //-calculates the address
