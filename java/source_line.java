c81c2da4d9c5a9b1216a5c4f55fad8668e34a3d4
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-11-05 20:20:05 -0500

    stage sourceline communication file

---
+++ java/source_line.java
@@ -0,0 +1,9 @@
+
+class source_line{
+	String symbol, mnemonic, label;
+	public source_line(String testty){
+		System.out.printf("Assembler communicating with source_line...  %s\n", testty);
+	}
+
+//addresses(pass1) opcode(pass2)-- output variables
+}
