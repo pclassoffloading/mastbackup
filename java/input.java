1e0db643e34eb9d47d1a6ad38dbcce589d75ac9b
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-11-08 19:01:18 -0500

    ifWeWantedTokenize

--- java/input.java
+++ java/input.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.io.File; //boolean createNewFile() This method atomically creates a new, empty file named by this abstract pathname if and only if a file with this name does not yet exist.
 import java.util.Scanner;
+import java.util.StringTokenizer;
 public class input{
 File file1;
 String filly = "input.txt";
@@ -8,7 +9,7 @@ String filly = "input.txt";
 	public input(String testty) throws IOException{
 
 		this.file1 = readFile(testty);
-
+		//ifWeWantedTokenize();
 	}
 
 	public File readFile(String filly) throws IOException{
@@ -35,14 +36,23 @@ String filly = "input.txt";
 		while (s.hasNext()){
 			//.next();
 			String word = s.next();
-			System.out.printf("Line who knows! WORD %d%s\n", count, word);
+			//System.out.printf("Line who knows! WORD %d%s\n", count, word);
 			count++;
 		//.useDelimiter("\\s*fish\\s*");
 
 		}
 	}
 	public void understand_line_number(){
-		
+
+	}
+	public void ifWeWantedTokenize(){
+		StringTokenizer st = new StringTokenizer("	this 	is a test");
+		int t = 0;
+		 while (st.hasMoreTokens()) {
+			 t++;
+				 System.out.println(st.nextToken());
+				 System.out.println(t);
+		 }
 	}
 }
 
