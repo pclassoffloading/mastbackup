f4e51fd3efa264381ef9ad45d938f03e07267d14
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-11-21 10:02:56 -0500

    cleanup input file

--- java/.old/input_old.java
+++ java/.old/input_old.java
@@ -16,37 +16,88 @@ source_line[] sourcelines;
 		this.fileInput0.close();this.fileInput0 = new Scanner(file1); 
 		readEachLine(fileInput0);
 
+
+
+		//this.file1 = readFile(testty);
+		//this.fileInput0 = new Scanner(file1);
+		//int countiy = countFile(fileInput0);
+		//sourcelines= new source_line[countiy]; //instantiated sourcelines
+		//readEachLine(fileInput1);
+		//return file1;
+		//ifWeWantedTokenize();
 	}
 
+	public File readFile(String filly) throws IOException{
+		this.file1 = new File(filly);
+		return file1;
+	}
+	public int countFile(Scanner fileInput0){
+		int count = 0;
+		while (fileInput0.hasNext()){
+			fileInput0.nextLine();
+			count++;
+		}
+		return count;
+	}
 	public void readEachLine(Scanner fileInput1)throws IOException{
 		while (fileInput1.hasNext()){
 			readEachWord(fileInput1.nextLine());
+//			System.out.println(fileInput1.nextLine());
 		}
 		fileInput1.close();
 	}
 
+	//TODO: Deliminate using scanner by spaces or tabs...
+	//TODO:
 	public void readEachWord(String trythis) throws IOException{
 		Scanner s = new Scanner(trythis).useDelimiter("\\s+");
 		count_words(s);
 		build_sourcelines(s);
-	}
 
+
+	}
 	public void build_sourcelines(Scanner s){
 		int count = 0;
 		while (s.hasNext()){
+		//.next();
 			String word = s.next();
+		//		System.out.printf("Line who knows! WORD %d%s\n", count, word);
 			count++;
+			//understand_word_number();
+			//.useDelimiter("\\s*fish\\s*");
 		}
 	}
 	public int count_words(Scanner s){
 		int count = 0;
 		while (s.hasNext()){
+			//.next();
 			String word = s.next();
 			System.out.printf("Line who knows! WORD %d%s\n", count, word);
 			count++;
+			//.useDelimiter("\\s*fish\\s*");
 		}s.reset();return count;
 	}
 	public void understand_word_number(int t, Scanner s){
 
 	}
 }
+//	public void ifWeWantedTokenize(){
+//		StringTokenizer st = new StringTokenizer("	this 	is a test");
+//		int t = 0;
+//		 while (st.hasMoreTokens()) {
+//			 t++;
+//				 System.out.println(st.nextToken());
+//				 System.out.println(t);
+//		 }
+//	}
+//}
+
+//import java.io.PrintWriter;
+//File output = new File("output.txt");
+//if (new File("output.txt").isFile()) {
+//	System.out.println();
+//} else {
+//	try {
+//		output.createNewFile();
+//		PrintWriter printer = new PrintWriter(output);
+//		printer.print(
